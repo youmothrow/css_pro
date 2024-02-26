3424f26cc7aad4a9fcd0848cdf903e5c939ca030
youmothrow <chenjianjun997@163.com>
2024-02-26 14:42:24 +0800

    change home js file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,24 @@ import './Home.css';
 class Home extends Component {
     render(){
       return (
-        <div class="relative bg-gray-50">
-        <h1 class="text-black-300 text-center">Hello world</h1>
-        <a href="http://localhost:3000/" class="text-sky-500 hover:text-sky-600">Read the docs &rarr;</a>
+        <div class="
+          p-6 max-w-sm mx-auto 
+         bg-white rounded-xl 
+          shadow-lg flex items-center 
+          space-x-4">
+        <h1 class=" 
+          text-xl font-medium 
+          text-black
+          "
+        >
+          个人博客
+        </h1>
+        <a href="http://localhost:3000/" 
+          class="text-sky-500
+           hover:text-sky-600"
+        >
+          Read the docs &rarr;
+        </a>
         </div>
       )
     }
